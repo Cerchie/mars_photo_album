f2585516892ca04ea3ffb208e5b53903797bd1d6
Lucia Cerchie <luciacerchie@gmail.com>
2020-09-24 09:30:23 -0700

    starting test code on user views

---
+++ test_user_views.py
@@ -0,0 +1,60 @@
+"""User View tests."""
+
+# run these tests like:
+#
+# FLASK_ENV=production python -m unittest test_user_views.py
+
+
+from app import app, CURR_USER_KEY
+import os
+from unittest import TestCase
+
+from models import db, connect_db, Photos, User, Favorites
+from bs4 import BeautifulSoup
+
+#testing db below
+os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+
+#creating tables
+db.create_all()
+
+# not testing wtf csrf
+
+app.config['WTF_CSRF_ENABLED'] = False
+
+class UserViewTestCase(TestCase):
+    """Test views for users, pattern matching from warbler project"""
+
+    def setUp(self):
+        """creating test client and adding sample data"""
+
+        db.drop_all()
+        db.create_all()
+
+        self.client = app.test_client()  # creating test client
+
+        self.testuser = User.signup(username="testuser",  # creating test user instance
+                                    password="testuser",)
+
+        self.testuser_id = 8989  # creating a user id
+        self.testuser.id = self.testuser_id  # setting the user id to our test user
+
+        self.u1 = User.signup("abc", "test1@test.com",
+                              "password", None)  # creating test user 1
+        self.u1_id = 778  # creating a user id
+        self.u1.id = self.u1_id  # setting the user id to our test user
+        self.u2 = User.signup("efg", "test2@test.com",
+                              "password", None)  # creating test user 2
+        self.u2_id = 884  # creating a user id
+        self.u2.id = self.u2_id  # setting the user id to our test user
+        # creating test user 3, no id
+        self.u3 = User.signup("hij", "test3@test.com", "password", None)
+        # creating test user 4, no id
+        self.u4 = User.signup("testing", "test4@test.com", "password", None)
+
+        db.session.commit()
+
+    def tearDown(self):
+        resp = super().tearDown()
+        db.session.rollback()
+        return resp
