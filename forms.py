2cf1ef6aa08b3b672f85df6065b14a42c223d841
Lucia Cerchie <luciacerchie@gmail.com>
2020-10-06 16:58:48 -0700

    added validator to all forms

--- forms.py
+++ forms.py
@@ -6,18 +6,18 @@ from wtforms.validators import DataRequired, Length
 class SignUpForm(FlaskForm):
     """form for signing up"""
 
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Username', validators=[Length(max=30),DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6), DataRequired()])
 
 class UserEditForm(FlaskForm):
     """form for editing user"""
 
-    username = StringField('New Username', validators=[DataRequired()])
+    username = StringField('New Username', validators=[Length(max=30),DataRequired()])
     password = PasswordField('Enter your password to confirm', validators=[Length(min=6), DataRequired()])
     
 
 class LoginForm(FlaskForm):
     """Login form."""
 
-    username = StringField('Username', validators=[Length(min=6, max=29, message="Please enter a username between 6 and 29 characters"),DataRequired()])
+    username = StringField('Username', validators=[Length(max=30),DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6), DataRequired()])
